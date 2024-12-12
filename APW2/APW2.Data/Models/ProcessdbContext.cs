b2f65e58f065248f827c7011bf18c3a91e392631
AleMendezS <98509409+AleMendezS@users.noreply.github.com>
2024-12-11 19:09:55 -0600

    Crear proyecto

---
+++ APW2/APW2.Data/Models/ProcessdbContext.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APW2.Data.Models;
+
+public partial class ProcessdbContext : DbContext
+{
+    public ProcessdbContext()
+    {
+    }
+
+    public ProcessdbContext(DbContextOptions<ProcessdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TaskManager> TaskManagers { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=Desktop\\SQLEXPRESS;Database=Processdb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskManager>(entity =>
+        {
+            entity.HasKey(e => e.TaskId).HasName("PK__TaskMana__7C6949D1B6EDA4EC");
+
+            entity.ToTable("TaskManager");
+
+            entity.Property(e => e.TaskId).HasColumnName("TaskID");
+            entity.Property(e => e.AssignedTo)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Category)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.CompletedDate).HasColumnType("datetime");
+            entity.Property(e => e.CreatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Description)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.DueDate).HasColumnType("datetime");
+            entity.Property(e => e.IsArchived).HasDefaultValue(false);
+            entity.Property(e => e.ModifiedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Notes)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.Priority)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasDefaultValue("Medium");
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasDefaultValue("Pending");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3213E83F7A22C53E");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__AB6E616466D44E69").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.IsActive)
+                .HasDefaultValue(true)
+                .HasColumnName("is_active");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ APW2/APW2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APW2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
